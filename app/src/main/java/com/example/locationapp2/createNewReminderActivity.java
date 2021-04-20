8e2e9890ed02f49091ef0fd06b742defc48987b7
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2021-04-20 11:26:16 -0400

    added good stuff to new reminder activity

--- app/src/main/java/com/example/locationapp2/createNewReminderActivity.java
+++ app/src/main/java/com/example/locationapp2/createNewReminderActivity.java
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.essam.simpleplacepicker.MapActivity;
 import com.essam.simpleplacepicker.utils.SimplePlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.w3c.dom.Text;
 
 public class createNewReminderActivity extends AppCompatActivity {
+
+    LatLng latLng = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_reminder);
+        Bundle bundle = getIntent().getExtras();
+        TextView theLocation;
+
+        double lat = -1.0, lng = -1.0;
+        if(bundle.getString("lat") != null){
+            lat = Double.parseDouble(bundle.getString("lat"));
+        }
+        if(bundle.getString("long") != null){
+            lng = Double.parseDouble(bundle.getString("long"));
+        }
+        if(lat != -1.0 && lng != -1.0) {
+            latLng = new LatLng(lat, lng);
+            theLocation = findViewById(R.id.theLocation);
+            theLocation.setText(latLng.latitude + ", " + latLng.longitude);
+        }
     }
 
     public void backToHome(View view){
@@ -27,6 +48,12 @@ public class createNewReminderActivity extends AppCompatActivity {
     public void toMap(View view){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
     }
 
     public void placePicker(View view){
